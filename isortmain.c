ba230d49262b8158f7b7a6c84c2eb766cee3d950
OfirOvadia96 <ofirrr999@gmail.com>
2021-01-05 18:29:51 +0200

    first

---
+++ isortmain.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include "isort.h"
+#define SIZE 50
+
+int main() {
+	 int Array [SIZE] ; 
+	 printf("enter %d number to Array \n" , SIZE);
+	for(int i =0; i<SIZE; i++){
+		scanf("%d",Array+i);
+	}
+ 
+insertion_sort(Array, SIZE);
+printf("the sorted Array is : \n");
+printArray(Array,SIZE);
+ 
+printf("\n");
+
+ return 0;
+}
---
+++ txtfind.h
@@ -0,0 +1,11 @@
+
+#ifndef _TXTFIND_H
+#define _TXTFIND_H
+
+int Getline(char s[]);
+int getword(char w[]);
+int substring(char *str1 , char *str2);
+void print_lines(char *str);
+void print_similar_words(char *str);
+
+#endif
